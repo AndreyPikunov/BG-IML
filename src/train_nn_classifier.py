de9695ab0419fe5f609e4ff68dd2bcfa33f926c8
Andrey Pikunov <pikunov.andrew@gmail.com>
2022-10-14 17:21:12 +0400

    refactoring

--- src/train_nn_classifier.py
+++ src/train_nn_classifier.py
@@ -11,32 +11,27 @@ from torchvision import transforms
 
 import mlflow
 
-import sys
-
-sys.path.append("../src")
-
-from datasets import PaintingDatasetCombo as PaintingDataset
-from trainers import TrainerCombo as Trainer
+from datasets import PaintingDataset
+from trainers import Trainer
 from scorers import ScorerCombo as Scorer
 from models import NNClassifier
-from losses import ComboLoss
 
 from utils import load_config, get_abs_dirname, load_resnet
 
 
 def main(config):
 
-    device = torch.device(config["train_combo"].get("device", "mps"))
+    device = torch.device(config["train_nn_classifier"].get("device", "mps"))
     logging.info(f"device: {device}")
 
     filename_design = os.path.join(
         config["shared"]["project_root"],
-        config["train_combo"]["filename_design"],
+        config["train_nn_classifier"]["filename_design"],
     )
 
     ann = pd.read_csv(filename_design)
 
-    labels_use = config["train_combo"].get("labels_use")
+    labels_use = config["train_nn_classifier"].get("labels_use")
     if labels_use is not None:
         ann = ann[ann.label.isin(labels_use)]
     else:
@@ -44,8 +39,8 @@ def main(config):
 
     ann["label_code"] = ann.label.astype("category").cat.codes
 
-    model_name = config["train_combo"]["model_name"]
-    _, resnet_weights = load_resnet(model_name)
+    resnet_name = config["train_nn_classifier"]["params_embedder"]["resnet_name"]
+    _, resnet_weights = load_resnet(resnet_name)
 
     transform_resnet = resnet_weights.DEFAULT.transforms()
     transform_train = transforms.Compose(
@@ -60,24 +55,24 @@ def main(config):
         ]
     )
 
-    column_fold = config["train_combo"]["column_fold"]
+    column_fold = config["train_nn_classifier"]["column_fold"]
 
-    folds_use = config["train_combo"].get("folds_use")
+    folds_use = config["train_nn_classifier"].get("folds_use")
     if folds_use is None:
         folds_use = ann[column_fold].unique()
 
-    batch_size = config["train_combo"]["batch_size"]
+    batch_size = config["train_nn_classifier"]["batch_size"]
     folder_images = os.path.join(
         config["shared"]["project_root"],
-        config["train_combo"]["folder_images"],
+        config["train_nn_classifier"]["folder_images"],
     )
 
     keys = "folder_output_model", "folder_output_report"
     for key in keys:
         value = os.path.join(
-            config["shared"]["project_root"], config["train_combo"][key]
+            config["shared"]["project_root"], config["train_nn_classifier"][key]
         )
-        config["train_combo"][key] = value
+        config["train_nn_classifier"][key] = value
 
     score_best_folds = []
 
@@ -112,8 +107,6 @@ def main(config):
         )
         dataloader_test = DataLoader(dataset_test, batch_size=batch_size, shuffle=False)
 
-        embedding_size = config["train_combo"]["embedding_size"]
-
         ann_fold = dataset_train.ann
         code2label = (
             ann_fold[["label", "label_code"]]
@@ -125,23 +118,31 @@ def main(config):
         logging.info(f"n_classes: {n_classes}")
         mlflow.log_dict(code2label.to_dict(), "code2label.yml")
 
-        params_embedder=dict(
-            resnet_name=model_name,
-            embedding_size=embedding_size,
-            freeze_resnet_cnn=False,
-            freeze_resnet_fc=False
-        )
+        params_embedder = config["train_nn_classifier"]["params_embedder"]
+        mlflow.log_params(params_embedder)
 
         model = NNClassifier(n_classes, params_embedder=params_embedder)
         parameters = model.parameters()
         model = model.to(device)
 
+        mlflow.log_param("n_classes", n_classes)
+
+        lr = config["train_nn_classifier"]["optimizer"]["lr"]
+        weight_decay = config["train_nn_classifier"]["optimizer"].get("weight_decay", 0)
+        optimizer = torch.optim.Adam(parameters, lr=lr, weight_decay=weight_decay)
+
+        lr_final = config["train_nn_classifier"]["scheduler"]["lr_final"]
+        n_epochs = config["train_nn_classifier"]["n_epochs"]
+        gamma = np.power(lr_final / lr, 1 / n_epochs)
+        scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=gamma)
+
         mlflow.log_params(
             {
-                "model_name": model_name,
-                "embedding_size": embedding_size,
-                "batch_size": batch_size,
-                "n_classes": n_classes,
+                "optimizer": "Adam",
+                "lr": lr,
+                "weight_decay": weight_decay,
+                "scheduler": "ExponentialLR",
+                "gamma": gamma,
             }
         )
 
@@ -149,43 +150,19 @@ def main(config):
         mlflow.log_dict(label_weight.to_dict(), "label_weight.yml")
 
         kw_criterion = {
-            "classification_weight": config["train_combo"].get("classification_weight"),
-            "class_weights": torch.tensor(label_weight.values).float().to(device),
-            "label_smoothing": config["train_combo"].get("label_smoothing"),
+            "weight": torch.tensor(label_weight.values).float().to(device),
+            "label_smoothing": config["train_nn_classifier"].get("label_smoothing"),
         }
 
-        criterion = ComboLoss(**kw_criterion)
+        criterion = torch.nn.CrossEntropyLoss(**kw_criterion)
 
-        mlflow.log_params({
-            "classification_weight": criterion.CE_weight,
-            "label_smoothing": kw_criterion["label_smoothing"]
-        })
+        mlflow.log_params(kw_criterion)
 
-        lr = config["train_combo"]["optimizer"]["lr"]
-        optimizer_name = config["train_combo"]["optimizer"]["name"]
-
-        if optimizer_name == "Adam":
-            optimizer = torch.optim.Adam(parameters, lr=lr)
-        else:
-            raise NotImplementedError()
-
-        gamma = config["train_combo"]["scheduler"]["gamma"]
-        scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=gamma)
-
-        mlflow.log_params(
-            {
-                "optimizer": optimizer_name,
-                "lr": lr,
-                "scheduler": "ExponentialLR",
-                "gamma": gamma,
-            }
-        )
-
-        top_k_list = config["train_combo"]["top_k_list"]
+        top_k_list = config["train_nn_classifier"]["top_k_list"]
         scorer = Scorer(top_k_list, code2label=code2label)
         mlflow.log_param("top_k", top_k_list)
 
-        score_target = config["train_combo"]["score_target"]
+        score_target = config["train_nn_classifier"]["score_target"]
 
         trainer = Trainer(
             model=model,
@@ -193,7 +170,7 @@ def main(config):
             criterion=criterion,
             dataloader_train=dataloader_train,
             dataloader_test=dataloader_test,
-            params=config["train_combo"],
+            params=config["train_nn_classifier"],
             device=device,
             code2label=code2label,
             scheduler=scheduler,
@@ -233,7 +210,7 @@ if __name__ == "__main__":
     mlflow_tracking_uri = config["shared"]["mlflow_tracking_uri"]
     mlflow.set_tracking_uri(mlflow_tracking_uri)
 
-    mlflow_experiment = config["train_combo"].get("mlflow_experiment", "train_combo")
+    mlflow_experiment = config["train_nn_classifier"].get("mlflow_experiment", "train_nn_classifier")
     mlflow.set_experiment(mlflow_experiment)
 
     main(config)
--- src/trainers/Trainer.py
+++ src/trainers/Trainer.py
@@ -1,16 +1,14 @@
 import os
 
 import torch
+import numpy as np
+import pandas as pd
 from tqdm import trange
 import mlflow
-
-
-def get_device(device="cpu"):
-    if device == "cpu":
-        device = torch.device("cpu")
-    elif isinstance(device, str):
-        device = torch.device(device)
-    return device
+import plotly.express as px
+from umap import UMAP
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import make_pipeline
 
 
 class Trainer:
@@ -18,116 +16,172 @@ class Trainer:
         self,
         model,
         optimizer,
-        criterion,
-        scorer,
+        scheduler,
         dataloader_train,
         dataloader_test,
+        criterion,
+        scorer,
+        score_target,
         params,
-        device="cpu",
+        code2label,
+        device
     ):
 
-        self.device = get_device(device)
+        self.device = device
         self.model = model.to(device)
 
         self.optimizer = optimizer
         self.criterion = criterion
-        self.scorer = scorer
 
         self.dataloader = {"train": dataloader_train, "test": dataloader_test}
 
-        self.y_true = {"train": None, "test": None}
-
-        self.y_pred = {"train": None, "test": None}
-
         self.params = params
 
-    def predict_model(self, dataloader):
+        self.embeddings = {"train": None, "test": None}
+        self.trues = {"train": None, "test": None}
+        self.preds = {"train": None, "test": None}
+
+        self.code2label = code2label
+        self.scheduler = scheduler
+        self.scorer = scorer
+        self.score_target = score_target
+
+    def step(self, train=True):
+
+        step_name = "train" if train else "test"
 
         device = self.device
-        model = self.model
 
-        y_true_list = []
-        y_pred_list = []
+        dataloader = self.dataloader[step_name]
 
-        with torch.no_grad():
-            for X, y_true in dataloader:
-                X = X.to(device)
-                y_true = y_true.to(device)
-                y_pred = model(X)
-                y_true_list.append(y_true)
-                y_pred_list.append(y_pred)
+        loss_batches = []
 
-        y_true = torch.vstack(y_true_list)
-        y_pred = torch.vstack(y_pred_list)
+        embeddings = []
+        trues = []
+        preds = []
 
-        return y_pred, y_true
+        if train:
+            self.model.train()
 
-    def test_model(self):
+        for x, y in dataloader:
 
-        y_pred, y_true = self.predict_model(self.dataloader["test"])
-        self.y_pred["test"] = y_pred
-        self.y_true["test"] = y_true
+            x = x.to(device)
+            y = y.to(device)
 
-        loss = self.criterion(y_pred, y_true).item()
-        score = self.scorer(y_pred, y_true)
+            pred, embedding = self.model(x)
 
-        return loss, score
+            if train:
+                self.optimizer.zero_grad()
+
+            loss = self.criterion(pred, y)
+
+            if train:
+                loss.backward()
+                self.optimizer.step()
+
+            embeddings.append(embedding.detach())
+            trues.append(y.detach())
+            preds.append(pred.detach())
+
+            loss_batches.append(loss.item())
+
+        if train:
+            self.model.eval()
+
+        loss = np.mean(loss_batches)
+      
+        self.embeddings[step_name] = torch.concat(embeddings)
+        self.trues[step_name] = torch.concat(trues)
+        self.preds[step_name] = torch.concat(preds)
+
+        scores = self.scorer(
+            pred=self.preds[step_name],
+            true=self.trues[step_name],
+            embedding=self.embeddings[step_name]
+        )
+
+        return loss, scores
 
     def train_model(self):
+        return self.step()
 
-        y_true_list = []
-        y_pred_list = []
-
-        device = self.device
-        model = self.model
-        dataloader = self.dataloader["train"]
-        criterion = self.criterion
-        scorer = self.scorer
-        optimizer = self.optimizer
-
-        model.train()
-
-        for X, y_true in dataloader:
-            X = X.to(device)
-            y_true = y_true.to(device)
-
-            optimizer.zero_grad()
-            y_pred = model(X)
-
-            loss = criterion(y_pred, y_true)
-            loss.backward()
-            optimizer.step()
-
-            y_true_list.append(y_true)
-            y_pred_list.append(y_pred)
-
-        model.eval()
-
-        y_true = torch.vstack(y_true_list)
-        y_pred = torch.vstack(y_pred_list)
-        self.y_pred["train"] = y_pred
-        self.y_true["train"] = y_true
-
-        loss = criterion(y_pred, y_true).item()
-        score = scorer(y_pred, y_true)
-
-        return loss, score
+    def test_model(self):
+        with torch.no_grad():
+            return self.step(train=False)
 
     def train_epoch(self):
 
         objectives = {}
 
-        loss, score = self.train_model()
-        objectives["loss_train"] = loss
-        objectives["score_train"] = score
+        step_names = "train", "test"
+        steps = self.train_model, self.test_model
 
-        loss, score = self.test_model()
-        objectives["loss_test"] = loss
-        objectives["score_test"] = score
+        for step_name, step in zip(step_names, steps):
+
+            loss, scores = step()
+            objectives[f"loss_{step_name}"] = loss
+
+            for key, value in scores.items():
+                objectives[f"{key}_{step_name}"] = value
 
         return objectives
 
     def train(self):
+
+        params = self.params
+        n_epochs = params["n_epochs"]
+
+        mlflow.log_param("n_epochs", n_epochs)
+
+        save_model_checkpoints = params["save_model_checkpoints"]
+
+        score_test_best = -1e9
+
+        with trange(n_epochs) as t:
+            for epoch in t:
+
+                objectives = self.train_epoch()
+
+                lr = self.optimizer.param_groups[0]["lr"]
+                mlflow.log_metric("lr", lr, step=epoch)
+
+                if self.scheduler is not None:
+                    self.scheduler.step()
+
+                mlflow.log_metrics(objectives, step=epoch)
+
+                metric_test = f"{self.score_target}_test"
+                metric_train = f"{self.score_target}_train"
+
+                score_test = objectives[metric_test]
+                score_train = objectives[metric_train]
+
+                df = self.collect_embeddings()
+                self.update_plot_columns(df)
+
+                filename = f"predictions/data-{epoch:03d}.csv"
+                mlflow.log_text(df.to_csv(), filename)
+
+                for color in "label_true", "label_pred":
+                    fig = self.plot_embeddings(df, color)
+                    filename = f"figures/embeddings-{color}-{epoch:03d}.html"
+                    mlflow.log_figure(fig, filename)
+
+                is_best = score_test > score_test_best
+
+                if is_best:
+                    score_test_best = score_test
+
+                    mlflow.log_metric("score_test_best", step=epoch)
+
+                    if save_model_checkpoints:
+                        self.dump_model()
+
+                t.set_postfix(train=score_train, test=score_test, best=score_test_best)
+
+        return score_test_best
+
+    def dump_model(self):
 
         params = self.params
 
@@ -135,70 +189,92 @@ class Trainer:
         os.makedirs(folder_output_model, exist_ok=True)
 
         filename_save_model_st = os.path.join(folder_output_model, "model.st")
-        filename_save_model_pt = os.path.join(folder_output_model, "model.pt")
 
-        folder_output_progress = params["folder_output_report"]
-        os.makedirs(folder_output_progress, exist_ok=True)
+        model = self.model
+        model = model.to("cpu")
+        torch.save(model.state_dict(), filename_save_model_st)
+        mlflow.log_artifact(filename_save_model_st)
 
-        score_test_best = 0.0
+        filename_save_optimizer_st = os.path.join(folder_output_model, "optimizer.st")
+        torch.save(self.optimizer.state_dict(), filename_save_optimizer_st)
+        mlflow.log_artifact(filename_save_optimizer_st)
 
-        rows = []
-        n_epochs = params["n_epochs"]
+        model = model.to(self.device)
 
-        mlflow.log_param("n_epochs", n_epochs)
+    def collect_embeddings(self):
 
-        save_model_checkpoints = params["save_model_checkpoints"]
+        emb_train = self.embeddings["train"]
+        emb_test = self.embeddings["test"]
+        emb = torch.concat([emb_train, emb_test]).cpu().numpy()
 
-        with trange(n_epochs) as t:
-            for epoch in t:
+        columns = [f"emb_{i:03d}" for i in range(emb.shape[1])]
+        df = pd.DataFrame(emb, columns=columns)
 
-                objectives = self.train_epoch()
-                rows.append(objectives)
+        n_train = len(emb_train)
+        df["fold"] = "test"
+        df.loc[:n_train, "fold"] = "train"
 
-                mlflow.log_metrics(objectives, step=epoch)
+        true_train = self.trues["train"]
+        true_test = self.trues["test"]
+        true = torch.concat([true_train, true_test]).cpu()
 
-                score_test = objectives["score_test"]
-                score_train = objectives["score_train"]
+        pred_train = self.preds["train"]
+        pred_test = self.preds["test"]
+        pred = torch.concat([pred_train, pred_test]).cpu()
 
-                if score_test > score_test_best:
-                    score_test_best = score_test
+        proba_pred = torch.nn.functional.softmax(pred, dim=1)
+        proba_true = true
 
-                    mlflow.log_metrics(
-                        {
-                            "best_epoch": epoch,
-                            "best_score_test": score_test,
-                            "best_score_train": score_train,
-                        },
-                        step=epoch,
-                    )
+        for i, label in self.code2label.iteritems():
+            key = f"proba_pred_{label}"
+            df[key] = proba_pred[:, i]
+            key = f"proba_true_{label}"
+            df[key] = proba_true[:, i]
 
-                    y_pred, y_true = self.y_pred["test"], self.y_true["test"]
-                    report = self.scorer.report(y_pred, y_true)
+        df["label_code_pred"] = pred.argmax(dim=1)
+        df["label_code_true"] = true.argmax(dim=1)
 
-                    mlflow.log_text(
-                        report["classification_report_text"],
-                        "reports/best/classification_report.txt",
-                    )
+        df["label_pred"] = df["label_code_pred"].replace(self.code2label)
+        df["label_true"] = df["label_code_true"].replace(self.code2label)
 
-                    fig = report["confusion_matrix_figure"]
-                    mlflow.log_figure(
-                        fig,
-                        "figures/best/confusion_matrix.png",
-                    )
-                    del fig
+        df.sort_values("label_true", inplace=True)
 
-                    if save_model_checkpoints:
+        return df
 
-                        model = self.model
-                        model = model.to("cpu")
-                        torch.save(model.state_dict(), filename_save_model_st)
-                        mlflow.log_artifact(filename_save_model_st)
+    def update_plot_columns(self, df):
 
-                        model_scripted = torch.jit.script(model)
-                        model_scripted.save(filename_save_model_pt)
-                        mlflow.log_artifact(filename_save_model_pt)
-                        model = model.to(self.device)
+        columns = [c for c in df.columns if c.startswith("emb")]
+        X = df[columns].values
 
-                t.set_postfix(train=score_train, test=score_test, best=score_test_best)
+        n_dims = len(columns)
 
-        return score_test_best
+        if n_dims > 3:
+            reducer = UMAP(n_components=3, random_state=42)
+            pipeline_umap = make_pipeline(StandardScaler(), reducer)
+            Z = pipeline_umap.fit_transform(X)
+            df["x"] = Z[:, 0]
+            df["y"] = Z[:, 1]
+            df["z"] = Z[:, 2]
+
+        else:
+            for i in range(n_dims):
+                letter = "xyz"[i]
+                df[letter] = X[:, i]
+
+    def plot_embeddings(self, df, color="label_true"):
+
+        args = dict(
+            color=color,
+            symbol="fold",
+            category_orders={color: self.code2label.values},
+            width=800,
+            height=600,
+        )
+
+        if "z" in df:
+            fig = px.scatter_3d(df, x="x", y="y", z="z", **args)
+
+        else:
+            fig = px.scatter(df, x="x", y="y", **args)
+
+        return fig
